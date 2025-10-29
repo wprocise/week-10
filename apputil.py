918d62f8075dca235c2ed1333b3ab833e56607bd
Woods Procise <wprocise@iu.edu>
2025-10-29 19:19:57 -0400

    Load in CSV

--- apputil.py
+++ apputil.py
@@ -4,3 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 
+# Exercise #1
+# 1) Load the coffee analysis data from a CSV file
+data = pd.read_csv("https://raw.githubusercontent.com/leontoddjohnson/datasets/refs/heads/main/data/coffee_analysis.csv")
+
